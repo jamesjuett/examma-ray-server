a9066eba6a3730b64f7a5c1754e1c321ef2a457f
James Juett <jjuett@umich.edu>
2022-06-06 21:22:14 -0400

    increase runtime limit

--- src/frontend/CodeSubmissionComponent.ts
+++ src/frontend/CodeSubmissionComponent.ts
@@ -63,7 +63,7 @@ export class CodeSubmissionComponent implements ManualGradingSubmissionComponent
       checkpoints: [
         new EndOfMainStateCheckpoint("Passes Test Cases", (sim: Simulation) => {
           return !sim.hasAnyEventOccurred
-        }, "", 10000)
+        }, "", 30000)
       ],
       completionCriteria: COMPLETION_ALL_CHECKPOINTS,
       starterCode: "",
